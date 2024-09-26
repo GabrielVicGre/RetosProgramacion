f9f96899904331408dfbfc5c70084afceee7542b
GabrielVicGre <gabovicgre@gmail.com>
2024-09-26 14:12:10 -0600

    Primer commit

--- src/Principal.java
+++ src/Principal.java
@@ -7,6 +7,6 @@ public class Principal {
     public static void main(String[] args) {
        // FizzBuzz.Iniciar();
        // LenguajeHacker.Iniciar();
-        PasswordGenerator.Iniciar();
+       // PasswordGenerator.Iniciar();
     }
 }
